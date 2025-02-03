b1ae3cb632bb8f25dc3f9989dde3117d075847e5
Sam <sdharaway@gmail.com>
2025-02-03 17:54:53 +0900

    setup shadcn with tailwind and created theming with button to switch themes

--- app/convertor/page.tsx
+++ app/convertor/page.tsx
@@ -1,7 +1,7 @@
 export default async function Convertor() {
   return (
     <div>
-      <ul>
+      <ul className="float-start space-x-8 bg-stone-400">
         <h1>CONVERTOR LINK WORKING</h1>
       </ul>
     </div>
