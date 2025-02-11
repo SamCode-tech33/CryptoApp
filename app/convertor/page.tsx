1e824394617750667e89cd220203feada48f42ea
Sam <sdharaway@gmail.com>
2025-02-11 13:03:53 +0900

    switched from router to Link

--- app/convertor/page.tsx
+++ app/convertor/page.tsx
@@ -1,19 +1,19 @@
 "use client";
 import { Button } from "@/components/ui/button";
-import { useRouter } from "next/navigation";
+import Link from "next/link";
 
 export default function Convertor() {
-  const router = useRouter();
-
   return (
     <div>
       <div className="flex ml-16">
-        <Button onClick={() => router.push("/coins")} className="mr-4">
-          Coins
-        </Button>
-        <Button onClick={() => router.push("/convertor")} className="ml-4">
-          Convertor
-        </Button>
+        <div className="flex">
+          <Link href="/coins" className="ml-16">
+            <Button className="mr-4">Coins</Button>
+          </Link>
+          <Link href="/convertor">
+            <Button className="ml-4">Convertor</Button>
+          </Link>
+        </div>
       </div>
     </div>
   );
